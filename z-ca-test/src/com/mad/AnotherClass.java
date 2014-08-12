194dacad923ed06ed08494d2809739fd57f0903e
MAD <michal.adamczuk1@gmail.com>
2014-08-12 14:10:43 +0200

    More another class tweaking

--- z-ca-test/src/com/mad/AnotherClass.java
+++ z-ca-test/src/com/mad/AnotherClass.java
@@ -4,6 +4,7 @@ public class AnotherClass {
 	public void doStuff() {
 		System.out.println("Doing stuff...");
 		System.out.println("Doing more stuff...");
+		System.out.println("Almost done doing stuff...");
 
 	}
 }
